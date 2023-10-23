1980fb1128418117913ed7ef711149f324259d08
Trungbui02 <buihuytrung2002@gmail.com>
2023-10-23 11:34:31 +0700

    new

---
+++ src/main/java/com/example/movip/MovipApplication.java
@@ -0,0 +1,12 @@
+package com.example.movip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovipApplication.class, args);
+    }
+}
